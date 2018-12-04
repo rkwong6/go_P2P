8fbe01cb55a5834165c47fa17ba87378ee3b41ad
Ryan Wong <rkwong6@users.noreply.github.com>
2018-12-04 14:36:58 -0800

    Create p2p.go

---
+++ p2p.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"hash/fnv"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+    fmt.Println("Peer-to-peer Simulation in Go");
+}
