b632ef1b89f38c544eac5f4b895ffaf990a88b69
Varun <i.nurav@gmail.com>
2018-12-13 23:00:23 -0800

    Basic simulation with hardcoded inputs

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # go_p2p
+Bevin Tang
+Varun Iyengar
+Ryan Wong
+CPE 469: Final Project
 Distributing File Downloads in a Peer-to-Peer Network in Go
 
 We will be simulating how file download speeds improve in a peer-to-peer network vs. downloading a file from one machine.
--- p2p.go
+++ p2p.go
@@ -6,23 +6,30 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	_"gopkg.in/cheggaaa/pb.v1"
+	"time"
+	"io/ioutil"
+	"math/rand"
 )
 
 // Each node has a download speed, upload speed, standby status, and down status
 type node struct {
 	nodeID      int  // unique identifier
-	downloadSpd int  // of file from seeded nodes (in Mb/sec)
+	// downloadSpd int  // of file from seeded nodes (in Mb/sec)
 	uploadSpd   int  // of file to end user (in Mb/sec)
 	isStandby   bool // (as a seed) Node is either on standby (true) or in use (false)
 	isDown      bool // Node is no longer a seed if true; if down
 }
 
 /* Global variables */
-var numNodes int
-var dlRate int
-var errRate int
+var numNodes int = 10;
+var dlRate int = 10;
+var maxErrRate int = 10;
 
 func main() {
+	var nodeList []node;
+	rand.Seed(time.Now().UTC().UnixNano());
+
 
 	// Define usage statement
 	usage := "[1] Run Simulation\n" +
@@ -43,8 +50,15 @@ func main() {
 
 		switch {
 		case text == "1":
+			fmt.Println("Enter filepath: ")
+			text, _ := reader.ReadString('\n');
+			text = strings.TrimSuffix(text, "\n");
+			data, err := ioutil.ReadFile(text);
+			if err != nil {
+				fmt.Println("Invalid filepath provided");
+			}
 			fmt.Println("Running simulation...")
-			run_simulation()
+			run_simulation(data, nodeList)
 		case text == "2":
 			check_params()
 		case text == "3":
@@ -58,7 +72,9 @@ func main() {
 				os.Exit(2)
 			}
 			numNodes = numNodesVal
-			fmt.Println("# of Nodes successfully updated")
+			nodeList = make([]node, numNodes);
+			generateNodes(nodeList);
+			fmt.Println("# of Nodes successfully updated to ", len(nodeList))
 		case text == "4":
 			fmt.Print("Please specify download rate: ")
 			dlRateStr, _ := reader.ReadString('\n')
@@ -73,15 +89,15 @@ func main() {
 			fmt.Println("download rate successfully updated")
 		case text == "5":
 			fmt.Print("Please specify error/failure rate: ")
-			errRateStr, _ := reader.ReadString('\n')
-			errRateStr = strings.TrimSuffix(errRateStr, "\n")
-			errRateVal, err := strconv.Atoi(errRateStr)
+			maxErrRateStr, _ := reader.ReadString('\n')
+			maxErrRateStr = strings.TrimSuffix(maxErrRateStr, "\n")
+			maxErrRateVal, err := strconv.Atoi(maxErrRateStr)
 			if err != nil {
 				// handle error
 				fmt.Println(err)
 				os.Exit(2)
 			}
-			errRate = errRateVal
+			maxErrRate = maxErrRateVal
 			fmt.Println("Erorr/failure rate successfully updated")
 		default:
 			fmt.Println("Error: Invalid input, please refer to usage statement...")
@@ -91,8 +107,67 @@ func main() {
 }
 
 /* Handles main execution of simulation of a peer-to-peer network */
-func run_simulation() {
+func run_simulation(data []byte, nodeList []node) {
+	if(len(nodeList) == 0){
+		fmt.Printf("Error: No nodes on network");
+		return;
+	}
+	split := len(data)/len(nodeList);
+	remainder := len(data) % len(nodeList);
+	var nodeData []byte;
+	// bar := pb.StartNew(len(nodeList))
+	for i := range nodeList {
+		// bar.Increment()
+		if(i != 0) {
+			nodeData = data[0+i*split:split+i*split];
+		} else {
+			nodeData = data[0+i*split:split+i*split + remainder];
+		}
+		// fmt.Println("Node: ", i, " Data: ", nodeData, " Amount: ", len(nodeData));
+		uploadFile(nodeList[i], nodeData);
+		// time.Sleep(time.Millisecond)
+		//INSERT SIMULATION HERE
 
+	}
+	// bar.FinishPrint("The End!")
+	// fmt.Println(split, "    ", remainder);
+}
+
+func uploadFile(node_n node , nodeData []byte ) {
+	var lagTime = rand.Intn(maxErrRate);												//amount of intitial waiting prior to beginning upload
+	var uploadSpeed = rand.Intn(node_n.uploadSpd/2) + rand.Intn(node_n.uploadSpd/2);	//buffer for how much data to upload per local time unit
+	var local_time = 0;
+
+	var dataUploaded = 0;
+	var lower_offset = 0;
+	var upper_offset = len(nodeData);
+
+	if(len(nodeData) < uploadSpeed) {
+		upper_offset = len(nodeData);
+	} else {
+		upper_offset = uploadSpeed;
+	}
+
+	for dataUploaded <= len(nodeData){
+		if local_time > lagTime {
+			for i := lower_offset; i < upper_offset; i++ {
+				dataUploaded += 1;
+			}
+			upper_offset += uploadSpeed;
+			lower_offset += uploadSpeed;
+		} 
+		local_time += 1;
+
+		time.Sleep(time.Millisecond);
+	}
+
+	fmt.Println("Time to finish uploading", len(nodeData) , "bytes on node ", node_n.nodeID ," : ", local_time);
+}
+
+func generateNodes(nodeList []node) {
+	for i := 0; i < numNodes; i++ {
+		nodeList[i] = node{i, dlRate, false, false};
+	}
 }
 
 /* Prints the current parameters which are set to be used in execution of simulation */
@@ -101,5 +176,5 @@ func check_params() {
 	fmt.Println("Current Parameters For Simulation")
 	fmt.Print("# of Nodes:    ", numNodes, "\n")
 	fmt.Print("Download Rate: ", dlRate, "Mb/s\n")
-	fmt.Print("Error Rate:    ", errRate, "%\n")
+	fmt.Print("Error Rate:    ", maxErrRate, "%\n")
 }
